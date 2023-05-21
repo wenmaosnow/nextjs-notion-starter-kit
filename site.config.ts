3bfd8eb97a4d534b4559b9922412aa417b33c525
wenmaosnow <60126824+wenmaosnow@users.noreply.github.com>
2023-05-21 10:59:47 +0800

    Update site.config.ts
    换了页面

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '554b9ec7768149509324833dcfcad331',
+  rootNotionPageId: 'b2ee08c954a74f36ac9ba0cb4aba5e60',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
